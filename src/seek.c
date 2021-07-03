3d0d56a33b676eca054f0a10a5283b4dfe66a979
Laurie Harvey <laurieharvey1602@gmail.com>
2021-07-03 10:13:15 +0100

    Refactor

--- src/seek.c
+++ src/seek.c
@@ -24,16 +24,42 @@ int main( int argc, char *argv[] )
         if( strcmp( argv[cmd], "-o" ) == 0 )
         {
             off_t current_offset = lseek( fd, (int)strtol( argv[cmd + 1], 0, 16 ), SEEK_SET );
-            char buffer[100];
-            sprintf( buffer, "Seeked to position 0x%lx\n", current_offset );
-            ssize_t bytes_written = write( STDOUT_FILENO, buffer, strlen( buffer ) );
+            char buffer[27];
+            sprintf( buffer, "0x%llx: seek\n", current_offset );
+
+            ssize_t bytes_written = 0;
+            ssize_t total_bytes_written = 0;
+
+            while( total_bytes_written < strlen( buffer ) )
+            {
+                bytes_written = write( STDOUT_FILENO, buffer, strlen( buffer ) );
+                total_bytes_written += bytes_written;
+
+                if( bytes_written < 0 )
+                {                    
+                    return 1;
+                }
+            }
         }
         else if( strcmp( argv[cmd], "-a" ) == 0 )
         {
             off_t current_offset = lseek( fd, (int)strtol( argv[cmd + 1], 0, 16 ), SEEK_CUR );
-            char buffer[100];
-            sprintf( buffer, "Seeked to position 0x%lx\n", current_offset );
-            ssize_t bytes_written = write( STDOUT_FILENO, buffer, strlen( buffer ) );
+            char buffer[30];
+            sprintf( buffer, "0x%llx: advance\n", current_offset );
+
+            ssize_t bytes_written = 0;
+            ssize_t total_bytes_written = 0;
+
+            while( total_bytes_written < strlen( buffer ) )
+            {
+                bytes_written = write( STDOUT_FILENO, buffer, strlen( buffer ) );
+                total_bytes_written += bytes_written;
+
+                if( bytes_written < 0 )
+                {                    
+                    return 1;
+                }
+            }
         }
         else if( strcmp( argv[cmd], "-r" ) == 0 )
         {
@@ -41,13 +67,22 @@ int main( int argc, char *argv[] )
             void *buffer = malloc( bytes_to_read + 6 );
             strcpy( buffer, "Read: " );
             ssize_t bytes_read = read( fd, buffer + 6, bytes_to_read );
+
             ssize_t bytes_written = 0;
             ssize_t total_bytes_written = 0;
+            
             while( bytes_written != -1 && total_bytes_written < bytes_read )
             {
                 bytes_written = write( STDOUT_FILENO, buffer, bytes_read + 6 );
                 total_bytes_written += bytes_written;
+
+                if( bytes_written < 0 )
+                {                    
+                    free( buffer );
+                    return 1;
+                }
             }
+
             free( buffer );
         }
         else if( strcmp( argv[cmd], "-w" ) == 0 )
@@ -55,7 +90,7 @@ int main( int argc, char *argv[] )
             off_t offset = lseek( fd, 0, SEEK_CUR );
             ssize_t bytes_written = write( fd, argv[cmd + 1], strlen( argv[cmd + 1] ) );
             void *buffer = malloc( strlen( argv[cmd + 1] ) + 100 );
-            int result = sprintf( buffer, "Wrote %s at position 0x%lx\n", argv[cmd + 1], offset );
+            int result = sprintf( buffer, "Wrote %s at position 0x%llx\n", argv[cmd + 1], offset );
             bytes_written = write( STDOUT_FILENO, buffer, strlen( buffer ) );
             free( buffer );
         }
